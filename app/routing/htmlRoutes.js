43f682e43cc05e96ca8b0a172cee41a08e6dec93
Les Barchard <lbarchard@Less-MacBook-Pro.local>
2017-04-23 11:04:29 -0400

    got html routes setup

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,33 @@
+//file handles all HTML GET requests
+var express    = require('express');  
+var app        = express();
+var path       = require("path");
+     
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR HTML
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+var startHTML = function() {
+
+app.use(function(req, res, next) {
+    // do logging
+    console.log(req.originalUrl);
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public', 'home.html'));
+});
+
+app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public', 'survey.html'));
+});
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+};
+
+exports.startHTML = startHTML;
---
+++ server.js
@@ -0,0 +1,5 @@
+var html  = require("./app/routing/htmlRoutes.js");
+// var api = require("./app/routing/apiRoutes.js")
+
+html.startHTML(); //allows web pages to be served up
+// api.startAPI(); //allows interaction through API
