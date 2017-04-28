4e16558b994950f427c8e762b7aa60906ec43c56
Les Barchard <lbarchard@lawn-143-215-114-228.lawn.gatech.edu>
2017-04-27 21:51:08 -0400

    port 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express     = require('express')
 var app         = express(); 
 var html        = require("./app/routing/htmlRoutes.js");
 var api         = require("./app/routing/apiRoutes.js")
-var port = process.env.PORT || 80;        // set our port
+var port = process.env.PORT || 8080;        // set our port
 
 app.use('/api', api)
 app.use('/', html)
